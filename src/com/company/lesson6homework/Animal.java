806185a4456752cd74c4c5c47a82e019c41189ca
Dragneva-Victoria <dragneva.victoria1996@gmail.com>
2022-04-04 21:05:04 +0300

    Код ДЗ 6

---
+++ src/com/company/lesson6homework/Animal.java
@@ -0,0 +1,13 @@
+package com.company.lesson6homework;
+
+public abstract class Animal {
+    protected int maxRun;
+    protected int maxSwin;
+
+    public Animal(int maxRun, int maxSwin) {
+        this.maxRun = maxRun;
+        this.maxSwin = maxSwin;
+    }
+    public abstract boolean run (int distance);
+    public abstract boolean swin (int distance);
+}
---
+++ src/com/company/lesson6homework/TestAnimal.java
@@ -0,0 +1,23 @@
+package com.company.lesson6homework;
+
+import java.util.Arrays;
+
+public class TestAnimal {
+    public static void main(String[] args) {
+        Animal[] animals = testAnimal();
+
+        for (Animal animal : animals) {
+             animal.run(300);
+             animal.swin(10);
+        }
+    }
+    private static Animal [] testAnimal() {
+        return new Animal[] {
+                new Dog (700, 5),
+                new Dog(50, 11),
+                new Dog (900, 7),
+                new Cat(130, 3),
+                new Cat (15,4)
+        };
+    }
+}
